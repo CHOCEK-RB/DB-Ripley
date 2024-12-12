55aff0decdcdb9896977cd6f616eade1622bcba2
CHOCEK <choceksalas@gmail.com>
2024-12-12 12:50:59 -0500

    Designs products page

---
+++ pagina-ripley/resources/js/components/ProductList.jsx
@@ -0,0 +1,84 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import "../css/productsList.css"
+import axios from "axios";
+
+const PaginatedProducts = () => {
+  const [products, setProducts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const fetchProducts = async (page = 1) => {
+    try {
+      const response = await axios.post('api/products', {
+        page: page,
+      })
+      const { data, currentPage , totalPages} = response.data;
+      setProducts(data);
+      setCurrentPage(currentPage);
+      setTotalPages(totalPages);
+    } catch (error) {
+      console.error("Error al obtener los datos de la API", error);
+    }
+  };
+
+  useEffect(() => {
+    const page = new URLSearchParams(location.search).get("page") || 1;
+    fetchProducts(parseInt(page, 10));
+  }, [location.search]);
+
+
+  const handlePageChange = (page) => {
+    navigate(`/products?page=${page}`); 
+    fetchProducts(page);
+  };
+
+  const renderPageNumbers = () => {
+    const pageNumbers = [];
+    for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(
+        <li>
+          <button 
+            className={i === currentPage ? "pagination paginationSelect" : "pagination"}
+            key={i}
+            onClick={() => handlePageChange(i)}
+          >
+            {i}
+          </button>
+        </li>
+      );
+    }
+    return pageNumbers;
+  };
+
+  return (
+    <div>
+      <h1>Productos</h1>
+      <div className="productsContainer">
+        {products.map((product) => (
+          <div
+            key={product.ID}
+            className="productBox"
+            onClick={() => navigate(`/product/${product.ID}`)}
+          >
+            <div className="imageProduct">
+              <img src={`/src/${product.ID}.webp`}/>
+            </div>
+            <div className="description">
+              <h2 className="nameProduct">{product.nombre}</h2>
+              <h2 className="precioProduct">{product.precio}</h2>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="paginationBox">
+        {renderPageNumbers()}
+      </div>
+    </div>
+  );
+};
+
+export default PaginatedProducts;
--- pagina-ripley/resources/js/showProducts.jsx
+++ pagina-ripley/resources/js/showProducts.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import PaginatedProducts from "./src/ProductList";
+import PaginatedProducts from "./components/ProductList";
 
 ReactDOM.createRoot(document.getElementById("react-root")).render(
   <BrowserRouter>
